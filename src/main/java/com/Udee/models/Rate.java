641938c4140906b19507693d0aca4c92a303cb0a
Kessler1459 <kessler1459@gmail.com>
2021-06-07 18:36:23 -0300

    Update Rate.java

--- src/main/java/com/Udee/models/Rate.java
+++ src/main/java/com/Udee/models/Rate.java
@@ -1,10 +1,7 @@
 package com.Udee.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 import javax.persistence.*;
 import javax.validation.constraints.Min;
@@ -16,6 +13,8 @@ import java.util.List;
 @Setter
 @ToString
 @RequiredArgsConstructor
+@Builder
+@AllArgsConstructor
 @Entity
 @Table(name = "rates")
 public class Rate {
