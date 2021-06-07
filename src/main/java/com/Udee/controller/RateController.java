595a03dc55d1d2fdd1dec7a3c02c021ca42d03dc
Kessler1459 <kessler1459@gmail.com>
2021-06-07 18:56:12 -0300

    Update RateController.java

--- src/main/java/com/Udee/controller/RateController.java
+++ src/main/java/com/Udee/controller/RateController.java
@@ -15,7 +15,6 @@ import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.net.URI;
 import java.util.List;
 import static com.Udee.utils.CheckPages.checkPages;
